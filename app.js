ddd33d4780da0305203d04f7e60bda1b5348654e
gpsajal <sajaltesting@gmail.com>
2021-01-10 18:56:47 +0530

    add headers

--- app.js
+++ app.js
@@ -18,6 +18,13 @@ app.use(bodyParser.urlencoded({extended:false}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
+
 app.use('/users', userRoutes);
 
 // app.set('view engine', 'ejs');
