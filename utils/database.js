38ed40e299b5b696569a065521d44e31b9596dd8
Vishal <vishal.khanjan@gmail.com>
2021-01-13 23:04:53 +0530

    Update database.js

--- utils/database.js
+++ utils/database.js
@@ -1,8 +1,9 @@
 const mysql = require("mysql2");
+require('dotenv').config();
 const pool = mysql.createPool({
-    host:process.env.HOST_NAME,
-    user:process.env.USERNAME,
-    password:process.env.PASSWORD,
-    database:process.env.DBNAME
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'chatterboxsports'
 });
 module.exports = pool.promise();
