d256be07640e07001ab1bcdaebc8960189d05d2b
Vishal <vishal.khanjan@gmail.com>
2021-01-16 21:50:25 +0530

    DB configuration
    DB configuration

--- utils/database.js
+++ utils/database.js
@@ -1,9 +1,10 @@
 const mysql = require("mysql2");
 require('dotenv').config();
+require('dotenv').config();
 const pool = mysql.createPool({
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'chatterboxsports'
+    host:process.env.HOST_NAME,
+    user:process.env.USERNAME,
+    password:process.env.PASSWORD,
+    database:process.env.DBNAME
 });
 module.exports = pool.promise();
